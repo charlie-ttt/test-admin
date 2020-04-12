a99be83ea7438521112854dee9c2f5dc83ef3f76
Charlie <achanont@gmail.com>
2020-04-12 15:01:25 +0700

    fist init

---
+++ src/posts.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  List,
+  Datagrid,
+  SimpleList,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Edit,
+  SimpleForm,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+  Create,
+  Filter,
+} from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+
+const PostFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
+export const PostList = (props) => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  return (
+    <List filters={<PostFilter />} {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.title}
+          secondaryText={(record) => `${record.views} views`}
+          tertiaryText={(record) =>
+            new Date(record.published_at).toLocaleDateString()
+          }
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
+
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ''} </span>;
+};
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="id" />
+      </ReferenceInput>
+      <TextInput source="id" />
+      <TextInput source="title" />
+      <TextInput source="body" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
+);
